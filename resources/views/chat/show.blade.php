e2a7f9c52e8bad1e6bdeeb0ebac9faa5fba3c018
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-12-22 04:01:03 -0500

    Showing the Private Events Only to the Receiver and Sender

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -116,4 +116,16 @@
         window.axios.post('/chat/greet/' + id);
     }
 </script>
+
+<script>
+    Echo.private('chat.greet.{{ auth()->user()->id }}')
+        .listen('GreetingSent', (e) => {
+            let element = document.createElement('li');
+
+            element.innerText = e.message;
+            element.classList.add('text-success');
+
+            messagesElement.appendChild(element);
+        });
+</script>
 @endpush
