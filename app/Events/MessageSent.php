bc4425c42d54ca26cb8c73aeb246b8a0f5f9d79d
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-12-22 02:33:53 -0500

    Creating an Event on Sending Messages in the Chat

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        \Log::debug("{$this->user->name}: {$this->message}");
+
+        return new PresenceChannel('chat');
+    }
+}
